f3c8686f185772a884ae40773f6bde54ffbe0428
Marco Rogers <marco.rogers@gmail.com>
2011-01-23 00:08:12 -0500

    reorganize testing, ugly but more useful

--- test/rest.js
+++ test/rest.js
@@ -32,9 +32,11 @@
  *
  */
 
-var fs = require('fs'),
-    assert = require('assert'),
-    et = require('../lib/evented-twitter');
+var util = require('util')
+        , fs = require('fs')
+        , assert = require('assert')
+        , _ = require('../lib/underscore')._
+        , et = require('../lib/evented-twitter');
 
 var creds = JSON.parse(fs.readFileSync(__dirname + '/credentials.json'))
     , oauth = creds.oauth;
@@ -46,10 +48,46 @@ assert.ok(oauth);
 
 oauth.auth = 'oauth';
 
+function ets(err, noStack) {
+    if(!err) return '';
+    else if(err.statusCode && err.data) {
+        var data = JSON.parse(err.data);
+        err = new Error(data.error + '(' + err.statusCode + '): ' + data.request);
+        noStack = true;
+    }
+    return (err.name ? err.name + ': ' : '') +
+           (err.message ? err.message + '\n' : '') +
+           ( (!noStack && err.stack) ? err.stack : '');
+}
+
 function handler(name) {
     return function(err, data) {
-        assert.ifError(err, name);
-        assert.ok(data && data.length > 0);
+        try {
+            if(err) throw err;
+            assert.ok(data && (_.isArray(data) ? !!data.length : true));
+        } catch(e) {
+            console.error('<Twitter.' + name + '>\n', ets(e), '\n\n\n');
+        }
+    }
+}
+
+function batch(tests, opts) {
+    opts = opts || {};
+    opts.batchSize = opts.batchSize || 1;
+    opts.batchInt = opts.batchInt || 1000;
+    for(var i=0; i < opts.batchSize; i++) {
+        try {
+            tests[i]();
+        } catch(e) {
+            console.error(ets(e), '\n\n\n');
+        }
+    }
+
+    tests = _.rest(tests, i);
+    if(tests.length) {
+        setTimeout(function() {
+                       batch(tests, opts );
+                   }, opts.batchInt);
     }
 }
 
@@ -57,52 +95,67 @@ var data;
 var tweet;
 var t = new et.Twitter(creds);
 
+function testTimeline() {
     t.publicTimeline('json', oauth, handler('publicTimeline'));
+
+    t.homeTimeline('json', oauth, handler('homeTimeline'));
+
+    t.friendsTimeline('json', oauth, handler('friendsTimeline'));
+
+    t.userTimeline('json', oauth, handler('friendsTimeline'));
+
+    t.mentions('json', oauth, handler('friendsTimeline'));
+
+    t.retweetedByMe('json', oauth, handler('retweetedByMe'));
+
+    t.retweetedToMe('json', oauth, handler('retweetedToMe'));
+
+    t.retweetsOfMe('json', oauth, handler('retweetsOfMe'));
+}
+
+function testUser() {
+    t.show('json', oauth, handler('show'));
+
+    t.lookup('json', oauth, handler('lookup'));
+
+    t.search('json', oauth, handler('search'));
+
+    t.suggestions('json', oauth, handler('suggestions'));
+
+    t.suggestionsCategory('json', oauth, handler('suggestionsCategory'));
+
+    //t.profileImage('json', oauth, handler('profileImage'));
+
+    t.friends('json', oauth, handler('friends'));
     
-    t.homeTimeline('json', oauth, handler);
+    t.followers('json', oauth, handler('followers'));
+}
 
-    t.friendsTimeline('json', oauth, handler);
+function testTweet() {
+    t.show('json', oauth, handler('show'));
 
-    t.userTimeline('json', oauth, handler);
+    //t.update('json', oauth, handler('update'));
+
+    //t.destroy('json', oauth, handler('destroy'));
     
-    t.mentions('json', oauth, handler);
+    //t.retweet('json', oauth, handler('retweet'));
 
-    t.retweetedByMe('json', oauth, handler);
+    t.retweets('json', oauth, handler('retweets'));
+    
+    t.retweetedBy('json', oauth, handler('retweetedBy'));
 
-    t.retweetedToMe('json', oauth, handler);
+    t.retweetedByIds('json', oauth, handler('retweetedByIds'));
+}
 
-    t.retweetsOfMe('json', oauth, handler);
 
-    t.show('json', oauth, handler);
+function testDM() {
+    t.directMessages('json', oauth, handler('directMessages'));
 
-    t.update('json', oauth, handler);
+    t.directMessagesSent('json', oauth, handler('directMessagesSent'));
 
-    t.destroy('json', oauth, handler);
+    //t.directMessagesNew('json', oauth, handler('directMessagesNew'));
+    
+    //t.directMessagesDestroy('json', oauth, handler('directMessagesDestroy'));
+}
 
-    t.retweet('json', oauth, handler);
-
-    t.retweets('json', oauth, handler);
-
-    t.retweetedBy('json', oauth, handler);
-
-    t.retweetedByIds('json', oauth, handler);
-
-    t.friends('json', oauth, handler);
-
-    t.followers('json', oauth, handler);
-
-    t.lookup('json', oauth, handler);
-
-    t.search('json', oauth, handler);
-
-    t.suggestions('json', oauth, handler);
-
-    t.suggestionsCategory('json', oauth, handler);
-
-    t.directMessages('json', oauth, handler);
-
-    t.directMessagesSent('json', oauth, handler);
-
-    t.directMessagesNew('json', oauth, handler);
-
-    t.directMessagesDestroy('json', oauth, handler);
+batch([ testTimeline, testUser, testTweet, testDM ], { batchSize: 1 });
